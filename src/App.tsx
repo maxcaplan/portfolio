4f1f498dcb3f1f0b0ea6c6dc626c3af26f9f80da
maxcaplan <maxacaplan@gmail.com>
2022-07-29 10:06:46 -0300

    Added font styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import './css/App.css'
 function App() {
   return (
     <div className="App">
+      <p>Hello World</p>
+      <p><b>Hello World</b></p>
+      <p><i>Hello World</i></p>
+      <p><b><i>Hello World</i></b></p>
       <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
     </div>
   )
 }
