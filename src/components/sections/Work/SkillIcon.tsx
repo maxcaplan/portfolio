75177aa43292bc9c06d604f1d9b08586c7e8c21d
maxcaplan <maxacaplan@gmail.com>
2024-01-17 12:43:59 -0400

    Added tooltip to skill icon

--- src/components/sections/Work/SkillIcon.tsx
+++ src/components/sections/Work/SkillIcon.tsx
@@ -106,6 +106,7 @@ export default function SkillIcon(props: SkillIconProps) {
       target={icons[props.skill].href ? "_blank" : undefined}
       title={icons[props.skill].name}
       className={`
+	  group
 	  relative
 	  flex 
 	  items-center 
@@ -117,6 +118,42 @@ export default function SkillIcon(props: SkillIconProps) {
 	  `}
     >
       {icons[props.skill].element}
+      <span
+        className={`
+	  absolute
+	  -top-[100%] 
+	  left-1/2 
+	  -translate-x-1/2
+	  hidden
+	  group-hover:inline
+	  `}
+      >
+        <span
+          className={`
+		px-3
+	    py-1
+	    rounded
+	    text-sm
+	    bg-brand-gray-600
+	    shadow-brand-gray-950
+	    shadow-md
+		`}
+        >
+          {icons[props.skill].name}
+        </span>
+        <span className="flex justify-center w-full">
+          <svg
+            height="6.38248856"
+            width="14"
+            viewBox="0 0 320 145.88545"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            className="fill-brand-gray-600"
+          >
+            <path d="m 136.00529,135.93191 c 13.2714,13.2714 34.82415,13.2714 48.09555,0 L 319.99997,0.03279553 C 320.11405,-0.08130447 0,0.13895553 0,0.13895553 L 135.89912,136.03809 Z" />
+          </svg>
+        </span>
+      </span>
     </a>
   );
 }
