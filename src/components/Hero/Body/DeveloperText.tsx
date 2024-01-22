5d0d00f357f9e075353986754929dba6e291555a
maxcaplan <maxacaplan@gmail.com>
2024-01-22 14:08:23 -0400

    Added hero body animation

---
+++ src/components/Hero/Body/DeveloperText.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useState } from "react";
+import useTypewriter from "../../../utils/hooks/useTypewriter";
+
+interface DeveloperTextProps {
+	animationPaused?: boolean;
+	onAnimationDone?: () => void;
+}
+
+export default function DeveloperText(props: DeveloperTextProps) {
+	const [cursor_visible, set_cursor_visible] = useState(true);
+
+	const developer_target_text = "DEVELOPER";
+
+	const developer_text = useTypewriter(
+		developer_target_text,
+		75,
+		props.animationPaused,
+	);
+
+	// On developer_text changed
+	useEffect(() => {
+		if (developer_text == developer_target_text && cursor_visible) {
+			setTimeout(() => {
+				set_cursor_visible(false);
+				if (props.onAnimationDone != undefined) {
+					props.onAnimationDone();
+				}
+			}, 500);
+		}
+	}, [developer_text]);
+
+	return (
+		<h1
+			className={`
+					align-middle 
+					text-5xl 
+					leading-normal 
+					sm:text-7xl 
+					sm:leading-normal 
+					font-extrabold
+					`}
+		>
+			{developer_text}
+			{cursor_visible && (
+				<span className="text-6xl text-brand-white-200">█</span>
+			)}
+		</h1>
+	);
+}
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,78 +1,45 @@
 import CharacterNoise from "./Backgrounds/CharacterNoise";
+import HeroBody from "./Body";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 type HeroProps = {
-  className?: string;
+	className?: string;
 };
 
 const noise_seed = Date.now();
 
 function Hero(props: HeroProps) {
-  const good = () => {
-    const characters = [
-      { char: "g", hover: "hover:text-brand-purple" },
-      { char: "o", hover: "hover:text-brand-green" },
-      { char: "o", hover: "hover:text-brand-red" },
-      { char: "d", hover: "hover:text-brand-blue" },
-    ];
+	return (
+		<div
+			id="hero"
+			className={`
+			z-0 
+			relative 
+			w-full 
+			h-screen 
+			overflow-x-hidden 
+			max-h-full 
+			bg-brand-gray-800 
+			${props.className || ""}
+			`}
+		>
+			<div
+				id="hero-background"
+				className="z-0 absolute top-0 left-0 w-full h-full pointer-events-none"
+			>
+				<CharacterNoise animationSpeed={5} seed={noise_seed} />
+			</div>
 
-    return characters.map(({ char, hover }, idx) => {
-      return (
-        <span
-          key={idx}
-          className={`
-		  transition 
-		  [&:not(:hover)]:duration-500 
-		  cursor-pointer 
-		  font-bold 
-		  ${hover}`}
-        >
-          {char}
-        </span>
-      );
-    });
-  };
+			<div id="hero-container" className="z-10 flex flex-col w-full h-full p-6">
+				<Navbar />
 
-  return (
-    <div
-      id="hero"
-      className={`z-0 relative w-full h-screen max-h-full bg-brand-gray-800 ${
-        props.className || ""
-      }`}
-    >
-      <div
-        id="hero-background"
-        className="z-0 absolute top-0 left-0 w-full h-full pointer-events-none"
-      >
-        <CharacterNoise animationSpeed={5} seed={noise_seed} />
-      </div>
+				<HeroBody />
 
-      <div id="hero-container" className="z-10 flex flex-col w-full h-full p-6">
-        <Navbar />
-        <div id="hero-text" className="z-10 flex-grow flex items-center">
-          <div className="flex flex-col">
-            <h6 className="text-base md:text-xl italic text-brand-gray-300 leading-normal">
-              // I like to make things
-            </h6>
-
-            <h1 className="text-5xl leading-normal sm:text-7xl sm:leading-normal font-extrabold">
-              DEVELOPER
-            </h1>
-
-            <h6 className="text-base md:text-xl italic text-brand-gray-400 leading-normal">
-              // And I like to make them {good()}
-            </h6>
-
-            <h1 className="text-5xl leading-normal sm:text-7xl sm:leading-normal font-extrabold">
-              DESIGNER
-            </h1>
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
+				<Footer />
+			</div>
+		</div>
+	);
 }
 
 export default Hero;
