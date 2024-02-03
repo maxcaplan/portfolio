32fc4a5624e1b035b43d9c3d7e45bba7d02289cb
maxcaplan <maxacaplan@gmail.com>
2024-02-03 16:13:16 -0400

    Fixed icon links accessibility

--- server.js
+++ server.js
@@ -33,129 +33,127 @@ const host = options.host || "localhost";
  * @param req - The server request object
  */
 function logRequest(req) {
-  const protocol = chalk.bgBlueBright
-    .hex("#FFF")
-    .bold(` ${req.protocol.toUpperCase()} `);
-  const date_string = new Date().toLocaleString().replace(",", "");
-  const client_ip = chalk.yellow(req.ip.replace("::ffff:", ""));
-  const request = chalk.cyan(`${req.method} ${req.originalUrl}`);
-  return `${protocol} ${date_string} ${client_ip} ${request}`;
+	const protocol = chalk.bgBlueBright
+		.hex("#FFF")
+		.bold(` ${req.protocol.toUpperCase()} `);
+	const date_string = new Date().toLocaleString().replace(",", "");
+	const client_ip = chalk.yellow(req.ip.replace("::ffff:", ""));
+	const request = chalk.cyan(`${req.method} ${req.originalUrl}`);
+	return `${protocol} ${date_string} ${client_ip} ${request}`;
 }
 
 /**
  * Gets the IP address(s) of the server
  */
 function getIps() {
-  let net_faces = os.networkInterfaces();
+	let net_faces = os.networkInterfaces();
 
-  let ips = [];
+	let ips = [];
 
-  // Iterate over network interfaces
-  Object.values(net_faces).forEach((net_face) => {
-    if (net_face === undefined) return;
+	// Iterate over network interfaces
+	Object.values(net_faces).forEach((net_face) => {
+		if (net_face === undefined) return;
 
-    // Iterate over network interface info
-    net_face.forEach((net_face_info) => {
-      // Skip over internal and non-IPv4 addresses
-      if (net_face_info.family !== "IPv4" || net_face_info.internal) return;
+		// Iterate over network interface info
+		net_face.forEach((net_face_info) => {
+			// Skip over internal and non-IPv4 addresses
+			if (net_face_info.family !== "IPv4" || net_face_info.internal) return;
 
-      ips.push(net_face_info.address);
-    });
-  });
+			ips.push(net_face_info.address);
+		});
+	});
 
-  return ips;
+	return ips;
 }
 
 /** Creates an http server for serving an SSR app */
 async function createServer() {
-  const app = express();
+	const app = express();
 
-  let vite;
+	let vite;
 
-  if (!isProduction) {
-    // Use vite dev server if in development environment
-    console.log(chalk.blue("Starting server in development mode..."));
+	if (!isProduction) {
+		// Use vite dev server if in development environment
+		console.log(chalk.blue("Starting server in development mode..."));
 
-    // Create Vite server in middleware mode
-    vite = await createViteServer({
-      server: { middlewareMode: true },
-      appType: "custom", // Disables vites HTML serving logic
-    });
+		// Create Vite server in middleware mode
+		vite = await createViteServer({
+			server: { middlewareMode: true },
+			appType: "custom", // Disables vites HTML serving logic
+		});
 
-    app.use(vite.middlewares);
-  } else {
-    // Use sirv for static asset serving if in production environment
-    console.log(chalk.blue("Starting server in production mode..."));
+		app.use(vite.middlewares);
+	} else {
+		// Use sirv for static asset serving if in production environment
+		console.log(chalk.blue("Starting server in production mode..."));
 
-    app.use(sirv("dist/client", { gzip: true }));
-  }
+		app.use(sirv("dist/client", { gzip: true }));
+	}
 
-  app.use("*", async (req, res) => {
-    const url = req.originalUrl;
-    let template, render;
+	app.use("*", async (req, res) => {
+		const url = req.originalUrl;
+		let template, render;
 
-    try {
-      if (!isProduction) console.log(logRequest(req));
+		try {
+			if (!isProduction) console.log(logRequest(req));
 
-      if (!isProduction) {
-        // Read index.html
-        template = fs.readFileSync(
-          path.resolve(__dirname, "index.html"),
-          "utf8",
-        );
+			if (!isProduction) {
+				// Read index.html
+				template = fs.readFileSync(
+					path.resolve(__dirname, "index.html"),
+					"utf8",
+				);
 
-        // Apply vite HTML transforms
-        template = await vite.transformIndexHtml(url, template);
+				// Apply vite HTML transforms
+				template = await vite.transformIndexHtml(url, template);
 
-        render = (await vite.ssrLoadModule("/src/entry-server.tsx")).render;
-      } else {
-        template = fs.readFileSync(
-          path.resolve("dist/client/index.html"),
-          "utf-8",
-        );
+				render = (await vite.ssrLoadModule("/src/entry-server.tsx")).render;
+			} else {
+				template = fs.readFileSync(
+					path.resolve("dist/client/index.html"),
+					"utf-8",
+				);
 
-        render = (await import("./dist/server/entry-server.js")).render;
-      }
+				render = (await import("./dist/server/entry-server.js")).render;
+			}
 
-      // Render app html
-      const appHtml = await render({ path: url });
+			// Render app html
+			const appHtml = await render({ path: url });
 
-      // Inject rendered app html into index.html template
-      const html = template.replace("<!--ssr-outlet-->", appHtml);
+			// Inject rendered app html into index.html template
+			const html = template.replace("<!--ssr-outlet-->", appHtml);
 
-      // Send rendered html to client
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/html").end(html);
-    } catch (e) {
-      if (!isProduction) vite.ssrFixStacktrace(e);
-      console.error(e);
-      res.status(500).end(e);
-    }
-  });
+			// Send rendered html to client
+			res.statusCode = 200;
+			res.setHeader("Content-Type", "text/html").end(html);
+		} catch (e) {
+			if (!isProduction) vite.ssrFixStacktrace(e);
+			console.error(e);
+			res.status(500).end(e);
+		}
+	});
 
-  return app;
+	return app;
 }
 
 // Create server and start listening for http requests
 createServer().then((app) => {
-  app.listen(port, host);
+	app.listen(port, host);
 
-  if (isProduction) return;
+	const server_host = host === "0.0.0.0" ? getIps()[0] : host;
 
-  const server_host = host === "0.0.0.0" ? getIps()[0] : host;
+	let start_message = `${chalk.bold("- Local:")} http://localhost:${port}`;
+	if (host === "0.0.0.0")
+		start_message += `\n${chalk.bold("- Network:")} http://${server_host}:${port}`;
 
-  let start_message = `${chalk.bold("- Local:")} http://localhost:${port}`;
-  if (host === "0.0.0.0")
-    start_message += `\n${chalk.bold("- Network:")} http://${server_host}:${port}`;
-
-  console.log("");
-  console.log(
-    boxen(start_message, {
-      margin: 1,
-      padding: 1,
-      borderColor: "green",
-      title: "Server Started",
-      titleAlignment: "center",
-    }),
-  );
+	console.log("");
+	console.log(
+		boxen(start_message, {
+			margin: 1,
+			padding: 1,
+			borderColor: "green",
+			title: "Server Started",
+			titleAlignment: "center",
+		}),
+	);
 });
--- src/components/Hero/Navbar.tsx
+++ src/components/Hero/Navbar.tsx
@@ -4,83 +4,91 @@ import logoSmDark from "../../assets/logo/logo_sm_dark.svg";
 type NavbarProps = {};
 
 export function Navbar(_props: NavbarProps) {
-  type NavLink = {
-    name: string;
-    href: string;
-    hover_class: string;
-    hidden?: boolean;
-  };
+	type NavLink = {
+		name: string;
+		href: string;
+		hover_class: string;
+		hidden?: boolean;
+	};
 
-  /** Navigation link information for the navbar */
-  const nav_links: NavLink[] = [
-    {
-      name: "work",
-      href: "#work",
-      hover_class: "hover:underline hover:decoration-brand-blue",
-    },
-    {
-      name: "skills",
-      href: "#skills",
-      hover_class: "hover:underline hover:decoration-brand-green",
-      hidden: true,
-    },
-    {
-      name: "about",
-      href: "#about",
-      hover_class: "hover:underline hover:decoration-brand-purple",
-    },
-    {
-      name: "contact",
-      href: "#contact",
-      hover_class: "hover:underline hover:decoration-brand-red",
-    },
-  ];
+	/** Navigation link information for the navbar */
+	const nav_info: NavLink[] = [
+		{
+			name: "work",
+			href: "#work",
+			hover_class: "hover:underline hover:decoration-brand-blue",
+		},
+		{
+			name: "skills",
+			href: "#skills",
+			hover_class: "hover:underline hover:decoration-brand-green",
+			hidden: true,
+		},
+		{
+			name: "about",
+			href: "#about",
+			hover_class: "hover:underline hover:decoration-brand-purple",
+		},
+		{
+			name: "contact",
+			href: "#contact",
+			hover_class: "hover:underline hover:decoration-brand-red",
+		},
+	];
 
-  return (
-    <div id="navbar" className="z-50 w-full">
-      <div
-        id="navbar-container"
-        className="w-full h-full flex flex-row items-center"
-      >
-        <img
-          src={logoDark}
-          alt="logo"
-          id="navbar-logo"
-          className="hidden md:flex self-stretch h-8"
-        />
-        <img
-          src={logoSmDark}
-          alt="logo"
-          id="navbar-logo-sm"
-          className="flex md:hidden self-stretch h-8"
-        />
+	/**
+	 * Creates a list of nav links given an array of nav info
+	 * @param nav_info - The list of nav info to create the links for
+	 */
+	const nav_links = (nav_info: NavLink[]) => {
+		return nav_info.map((link, idx) => {
+			return (
+				<span
+					key={`link-${idx}`}
+					className={`group ${link.hidden ? "hidden" : ""}`}
+				>
+					<span className="hidden">&gt; </span>
+					<a
+						href={link.href}
+						className={`transtion duration-200 decoration-[1.5px] decoration-wavy underline-offset-4 hover:text-brand-white ${link.hover_class}`}
+					>
+						{link.name}
+					</a>
+				</span>
+			);
+		});
+	};
 
-        <div id="navbar-right" className="w-full flex flex-row justify-end">
-          <div
-            id="navbar-links"
-            className="flex flex-col items-end gap-4 no-underline text-brand-gray-200"
-          >
-            {nav_links.map((link, idx) => {
-              return (
-                <span
-                  key={`link-${idx}`}
-                  className={`group ${link.hidden ? "hidden" : ""}`}
-                >
-                  <span className="hidden">&gt; </span>
-                  <a
-                    href={link.href}
-                    className={`transtion duration-200 decoration-[1.5px] decoration-wavy underline-offset-4 hover:text-brand-white ${link.hover_class}`}
-                  >
-                    {link.name}
-                  </a>
-                </span>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div id="navbar" className="z-50 w-full">
+			<div
+				id="navbar-container"
+				className="w-full h-full flex flex-row items-center"
+			>
+				<img
+					src={logoDark}
+					alt="logo"
+					id="navbar-logo"
+					className="hidden md:flex self-stretch h-8"
+				/>
+				<img
+					src={logoSmDark}
+					alt="logo"
+					id="navbar-logo-sm"
+					className="flex md:hidden self-stretch h-8"
+				/>
+
+				<div id="navbar-right" className="w-full flex flex-row justify-end">
+					<div
+						id="navbar-links"
+						className="flex flex-col items-end gap-4 no-underline text-brand-gray-200"
+					>
+						{nav_links(nav_info)}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
