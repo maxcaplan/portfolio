6952fe4b34830b1b46d8ada22489f549ee48aa6a
maxcaplan <maxacaplan@gmail.com>
2024-02-03 22:46:34 -0400

    Fixed navbar always visible on mobile

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 // Hooks
@@ -17,38 +17,53 @@ import Contact from "../../components/Sections/Contact";
 import Work from "../../components/Sections/Work";
 
 function Home() {
-  const hero_ref = useRef<HTMLDivElement>(null);
-  const show_navbar = useIsSticky(hero_ref);
+	const hero_ref = useRef<HTMLDivElement>(null);
+	const is_hero_sticky = useIsSticky(hero_ref);
+	const [is_at_top, set_is_at_top] = useState(true);
 
-  // Scroll to section with matching url hash on page loaded
-  const location = useLocation();
-  useScrollToHash(location.hash);
+	// Scroll to section with matching url hash on page loaded
+	const location = useLocation();
+	useScrollToHash(location.hash);
 
-  return (
-    <PageWrapper>
-      <Navbar
-        homePage={true}
-        className={`fixed top-0 left-0 z-[100] opacity-0 transition-opacity duration-200 ${show_navbar ? "opacity-100" : "opacity-0 pointer-events-none"}`}
-      />
-      <div className="w-full h-full flex flex-col gap-y-20">
-        <div className="flex flex-col xl:grid xl:grid-cols-3 xl:grid-flow-row w-full border-b border-brand-gray-700">
-          <div className="relative xl:col-span-1 flex flex-row w-full border-b xl:border-r border-brand-gray-700">
-            <Hero ref={hero_ref} className="sticky top-0" />
-          </div>
+	// On component mounted
+	useEffect(() => {
+		set_is_at_top(window.scrollY === 0);
 
-          <div className="relative xl:col-span-2 flex flex-col gap-y-20 w-full pt-6 pb-20">
-            <Work />
+		// Update is at top state on scroll
+		window.onscroll = () => {
+			set_is_at_top(window.scrollY === 0);
+		};
 
-            <About />
-          </div>
-        </div>
+		return () => {
+			window.onscroll = null;
+		};
+	});
 
-        <Contact />
+	return (
+		<PageWrapper>
+			<Navbar
+				homePage={true}
+				className={`fixed top-0 left-0 z-[100] opacity-0 transition-opacity duration-200 ${is_hero_sticky ? "xl:opacity-0 xl:pointer-events-none" : "xl:opacity-100"} ${is_at_top ? "opacity-0 pointer-events-none" : "opacity-100"}`}
+			/>
+			<div className="w-full h-full flex flex-col gap-y-20">
+				<div className="flex flex-col xl:grid xl:grid-cols-3 xl:grid-flow-row w-full border-b border-brand-gray-700">
+					<div className="relative xl:col-span-1 flex flex-row w-full border-b xl:border-r border-brand-gray-700">
+						<Hero ref={hero_ref} className="sticky top-0" />
+					</div>
 
-        <Footer />
-      </div>
-    </PageWrapper>
-  );
+					<div className="relative xl:col-span-2 flex flex-col gap-y-20 w-full pt-6 pb-20">
+						<Work />
+
+						<About />
+					</div>
+				</div>
+
+				<Contact />
+
+				<Footer />
+			</div>
+		</PageWrapper>
+	);
 }
 
 export default Home;
--- src/utils/hooks/useIsSticky.ts
+++ src/utils/hooks/useIsSticky.ts
@@ -5,23 +5,23 @@ import { RefObject, useEffect, useState } from "react";
  * @param ref - A ref object for the element to observe
  */
 const useIsSticky = (ref: RefObject<HTMLElement>): boolean => {
-  const [is_sticky, set_is_sticky] = useState(false);
+	const [is_sticky, set_is_sticky] = useState(false);
 
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([e]) => set_is_sticky(e.intersectionRatio < 1),
-      { threshold: 1 },
-    );
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			([e]) => set_is_sticky(e.intersectionRatio === 1),
+			{ threshold: 1 },
+		);
 
-    // Observe element
-    if (ref.current) observer.observe(ref.current);
+		// Observe element
+		if (ref.current) observer.observe(ref.current);
 
-    return () => {
-      if (ref.current) observer.unobserve(ref.current);
-    };
-  }, []);
+		return () => {
+			if (ref.current) observer.unobserve(ref.current);
+		};
+	}, []);
 
-  return is_sticky;
+	return is_sticky;
 };
 
 export default useIsSticky;
