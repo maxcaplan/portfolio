093c218bbdc9b86317e4b8a7fb4f2318dc622a8b
maxcaplan <maxacaplan@gmail.com>
2024-02-05 13:06:55 -0400

    Ported prerender script to typescript

---
+++ prerender.ts
@@ -0,0 +1,113 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const toAbsolute = (p: string) => path.resolve(__dirname, p);
+const pages_dir = "src/pages";
+
+const template = fs.readFileSync(toAbsolute("dist/static/index.html"), "utf-8");
+const render = (await import("./dist/server/entry-server.js")).render;
+
+interface DynamicPage {
+	path: string;
+	params: string[];
+}
+
+const get_page_paths = (pages_dir: string) => {
+	/**
+	 * Returns true if directory name is the following format: [name]
+	 * @param dir - The directory to check
+	 */
+	const is_param_dir = (dir: string) => {
+		if (dir.length === 0) return false;
+		return dir.charAt(0) === "[" && dir.charAt(dir.length - 1) === "]";
+	};
+
+	/**
+	 * Returns an array of all the parameters in a path
+	 * @param path - The path to infer the parameters from
+	 */
+	const get_params_from_path = (dir_path: string) => {
+		return dir_path
+			.split("/")
+			.filter((dir) => is_param_dir(dir))
+			.map((dir) => dir.slice(1, dir.length - 1));
+	};
+
+	// Get all directory paths in pages_dir
+	const dirs = fs
+		.readdirSync(toAbsolute(pages_dir), {
+			recursive: true,
+			encoding: "utf8",
+		})
+		.filter((dir_path) => {
+			if (fs.statSync(path.join(pages_dir, dir_path)).isDirectory()) {
+				return dir_path;
+			}
+		});
+
+	// Filter out all direcories that don't contain an index.tsx file
+	const page_dirs = dirs.filter((dir_path) => {
+		return fs.existsSync(path.join(pages_dir, dir_path, "index.tsx"));
+	});
+
+	let static_pages: string[] = [];
+	let dynamic_pages: DynamicPage[] = [];
+
+	// Seperate static pages and dynamic pages
+	page_dirs.forEach((dir_path) => {
+		const params = get_params_from_path(dir_path);
+		if (params.length <= 0) static_pages.push(dir_path);
+		else dynamic_pages.push({ path: dir_path, params: params });
+	});
+
+	// Format static page paths
+	static_pages = static_pages.map((dir_path) => {
+		const name = dir_path.replace(/\.tsx$/, "").toLowerCase();
+		return name === "home" ? `/` : `/${name}`;
+	});
+
+	console.log(
+		`Found ${static_pages.length} static ${static_pages.length === 1 ? "page" : "pages"}`,
+	);
+
+	console.log(
+		`Found ${dynamic_pages.length} dynamic ${dynamic_pages.length === 1 ? "page" : "pages"}`,
+	);
+
+	return { static_pages, dynamic_pages };
+};
+
+/**
+ * Renders static pages from given array of paths
+ * @param paths - Array of paths to static pages to render
+ */
+const render_static_pages = async (paths: string[]) => {
+	console.log("Rendering static pages: \n");
+
+	paths.forEach(async (path) => {
+		console.log(`Rendering ${path}...`);
+
+		// Render app html
+		const appHtml = await render("http://localhost" + path);
+
+		// Inject app html into template html
+		const html = template.replace(`<!--ssr-outlet-->`, appHtml);
+
+		// Write html to file
+		const filePath = `dist/static${path === "/" ? "/index" : path}.html`;
+		fs.writeFileSync(toAbsolute(filePath), html);
+	});
+};
+
+// TODO: Implement dynamic route rendering
+const render_dynamic_pages = async (paths: DynamicPage[]) => {
+	// console.log("Rendering dynamic pages: \n");
+};
+
+(async () => {
+	const { static_pages, dynamic_pages } = get_page_paths(pages_dir);
+	await render_static_pages(static_pages);
+	await render_dynamic_pages(dynamic_pages);
+})();
