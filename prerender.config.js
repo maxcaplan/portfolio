15f6d58a97f062148b2e13583c7015edbb005b31
maxcaplan <maxacaplan@gmail.com>
2024-02-17 16:09:51 -0400

    Added sitemap generation to prerendering

--- prerender.config.js
+++ prerender.config.js
@@ -1,4 +1,6 @@
 export const Config = {
+  domain: "www.maxcaplan.com",
+  protocol: "https://",
   title: "Max Caplan - Portfolio | Developer && Designer",
   description:
     "Hi, I'm Max Caplan. I'm a full stack web developer and designer with over 3 years of freelance experience and a lifetime of passion for building things!",
--- prerender.ts
+++ prerender.ts
@@ -16,6 +16,10 @@ const template = fs.readFileSync(toAbsolute("dist/static/index.html"), "utf-8");
 const render = (await import("./dist/server/entry-server.js")).render;
 
 interface Config {
+  /** Website domain */
+  domain?: string;
+  /** Website protocol (http://, https://) */
+  protocol?: string;
   /** Base page title */
   title?: string;
   /** Page description */
@@ -143,60 +147,20 @@ const inject_meta = (
 /**
  * Renders static pages from given array of paths
  * @param paths - Array of paths to static pages to render
+ * @param config - Prerenderer config object
  */
-const render_static_pages = async (paths: string[]) => {
+const render_static_pages = async (paths: string[], config: Config) => {
   console.log("\nRendering static pages: \n");
 
-  paths.forEach(async (page_path) => {
-    console.log(`Rendering ${page_path} ...`);
-
-    // Render app html
-    const appHtml = await render("http://localhost" + page_path);
-
-    // Inject meta data into template html
-    let html = inject_meta(template, config, page_path);
-
-    // Inject app html into template html
-    html = html.replace(`<!--ssr-outlet-->`, appHtml);
-
-    // Write html to file
-    const filePath = `dist/static${page_path === "/" ? "/index" : page_path}.html`;
-    fs.writeFileSync(toAbsolute(filePath), html);
-  });
-};
-
-/**
- * Renders dynamic pages from given array of paths
- * @param paths - Array of paths to dynamic pages to render
- */
-const render_dynamic_pages = async (paths: DynamicPage[]) => {
-  console.log("\nRendering dynamic pages: \n");
-
-  // Open vite server
-  const vite = await createViteServer();
-
-  paths.forEach(async (dynamic_path) => {
-    const root = dynamic_path.path.split("/")[0];
-    const root_path = "/" + root.replace(/\.tsx$/, "").toLowerCase();
-
-    // Load array of all content for path
-    const content_arr = (
-      await vite.ssrLoadModule(`./src/content/${root}/index.ts`)
-    ).default as Content[];
-
-    // Create directory for rendered pages
-    fs.mkdirSync(toAbsolute("dist/static" + root_path), { recursive: true });
-
-    content_arr.forEach(async (content) => {
-      const page_path = `${root_path}/${content.id}`;
-
+  await Promise.all(
+    paths.map(async (page_path) => {
       console.log(`Rendering ${page_path} ...`);
 
       // Render app html
       const appHtml = await render("http://localhost" + page_path);
 
       // Inject meta data into template html
-      let html = inject_meta(template, config, page_path, content);
+      let html = inject_meta(template, config, page_path);
 
       // Inject app html into template html
       html = html.replace(`<!--ssr-outlet-->`, appHtml);
@@ -204,15 +168,148 @@ const render_dynamic_pages = async (paths: DynamicPage[]) => {
       // Write html to file
       const filePath = `dist/static${page_path === "/" ? "/index" : page_path}.html`;
       fs.writeFileSync(toAbsolute(filePath), html);
-    });
-  });
+
+      return;
+    }),
+  );
+};
+
+/**
+ * Renders dynamic pages from given array of paths
+ * @param paths - Array of paths to dynamic pages to render
+ * @param config - Prerenderer config object
+ */
+const render_dynamic_pages = async (paths: DynamicPage[], config: Config) => {
+  console.log("\nRendering dynamic pages: \n");
+
+  // Open vite server
+  const vite = await createViteServer();
+
+  await Promise.all(
+    paths.map(async (dynamic_path) => {
+      const root = dynamic_path.path.split("/")[0];
+      const root_path = "/" + root.replace(/\.tsx$/, "").toLowerCase();
+
+      // Load array of all content for path
+      const content_arr = (
+        await vite.ssrLoadModule(`./src/content/${root}/index.ts`)
+      ).default as Content[];
+
+      // Create directory for rendered pages
+      fs.mkdirSync(toAbsolute("dist/static" + root_path), { recursive: true });
+
+      content_arr.forEach(async (content) => {
+        const page_path = `${root_path}/${content.id}`;
+
+        console.log(`Rendering ${page_path} ...`);
+
+        // Render app html
+        const appHtml = await render("http://localhost" + page_path);
+
+        // Inject meta data into template html
+        let html = inject_meta(template, config, page_path, content);
+
+        // Inject app html into template html
+        html = html.replace(`<!--ssr-outlet-->`, appHtml);
+
+        // Write html to file
+        const filePath = `dist/static${page_path === "/" ? "/index" : page_path}.html`;
+        fs.writeFileSync(toAbsolute(filePath), html);
+      });
+    }),
+  );
 
   // Close vite server
   vite.close();
 };
 
+/**
+ * Generates a sitemap for static and dynamic paths
+ * @param static_paths - Static site paths
+ * @param dynamic_paths - Dynamic site paths
+ * @param config - Prerenderer config object
+ */
+const generate_sitemap = async (
+  static_paths: string[],
+  dynamic_paths: DynamicPage[],
+  config: Config,
+) => {
+  console.log("\nGenerating sitemap...\n");
+
+  /** Wrapper for xml sitemap */
+  const sitemap_wrapper = (body?: string): string => {
+    return `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${body || ""}
+</urlset>`;
+  };
+
+  /** Wrapper for xml sitemap url */
+  const url_wrapper = (location: string): string => {
+    return `	<url>
+		<loc>${location}</loc>
+		<lastmod>${new Date().toISOString()}</lastmod>
+	</url>`;
+  };
+
+  // Create location root from config
+  let location_root = `${config.protocol || "http://"}${config.domain || "localhost"}`;
+  // Ensure root dosn't end with '/'
+  if (location_root.charAt(location_root.length - 1) == "/")
+    location_root = location_root.substring(0, location_root.length - 1);
+
+  // Create xml sitemap url tags for static pages
+  const static_urls = static_paths.map((page_path) => {
+    return url_wrapper(location_root + page_path);
+  });
+
+  // Open vite server
+  const vite = await createViteServer();
+
+  // Create xml sitemap url tags for dynamic pages
+  const dynamic_urls = (
+    await Promise.all(
+      dynamic_paths.map(async (dynamic_path) => {
+        const root = dynamic_path.path.split("/")[0];
+        const root_path = "/" + root.replace(/\.tsx$/, "").toLowerCase();
+
+        // Load array of all content for path
+        const content_arr = (
+          await vite.ssrLoadModule(`./src/content/${root}/index.ts`)
+        ).default as Content[];
+
+        return content_arr.map((content) => {
+          const page_path = `${root_path}/${content.id}`;
+          return url_wrapper(location_root + page_path);
+        });
+      }),
+    )
+  ).flat();
+
+  // Create sitemap
+  const urls: string[] = [...static_urls, ...dynamic_urls];
+  const sitemap = sitemap_wrapper(urls.join("\n"));
+
+  // Create directory for sitemap
+  fs.mkdirSync(toAbsolute("dist/static/"), { recursive: true });
+  // Write sitemap to file
+  fs.writeFileSync(toAbsolute("dist/static/sitemap.xml"), sitemap);
+
+  vite.close();
+
+  return;
+};
+
 (async () => {
+  const start_time = Date.now();
+
   const { static_pages, dynamic_pages } = get_page_paths(pages_dir);
-  await render_static_pages(static_pages);
-  await render_dynamic_pages(dynamic_pages);
+  await render_static_pages(static_pages, config);
+  await render_dynamic_pages(dynamic_pages, config);
+  await generate_sitemap(static_pages, dynamic_pages, config);
+
+  const end_time = Date.now();
+  console.log(
+    `Prerender finished in ${Math.ceil((end_time - start_time) / 1000)}s`,
+  );
 })();
