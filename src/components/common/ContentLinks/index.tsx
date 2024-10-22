58a6e5d8153bd68b31ee00fe098c2f52d245cb6e
maxcaplan <maxacaplan@gmail.com>
2024-10-22 18:21:36 -0300

    Fixed content links text

--- src/components/common/ContentLinks/index.tsx
+++ src/components/common/ContentLinks/index.tsx
@@ -28,7 +28,7 @@ const ContentLinks = (props: { sourceHref?: string, demoHref?: string }) => {
   if (props.demoHref !== undefined && props.demoHref !== "")
     links.push(
       <ContentLink href={props.demoHref}>
-        source
+        demo
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-terminal" > <polyline points="4 17 10 11 4 5" > </polyline><line x1="12" y1="19" x2="20" y2="19"></line > </svg>
       </ContentLink>
     )
