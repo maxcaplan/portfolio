f07cddf023ca34647e095784f1da317adcb9a1f1
maxcaplan <maxacaplan@gmail.com>
2024-10-22 18:11:56 -0300

    Added footer easter egg

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,46 @@
+import { useState } from "react";
+
 /** Page footer component */
 export default function Footer() {
+  let [emoticons, set_emoticons] = useState<string>(":) :( :/ :O")
+  let [emoticon_timer, set_emoticon_timer] = useState<NodeJS.Timeout | undefined>()
+  let [emoticon_is_mouse_over, set_emoticon_is_mouse_over] = useState(false)
+
+  const random_emoticons = (emoticons: string[]) => {
+    // Randomize the emoticon array using the Durstenfeld shuffle
+    for (var i = emoticons.length - 1; i >= 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = emoticons[i];
+      emoticons[i] = emoticons[j];
+      emoticons[j] = temp;
+    }
+
+    return emoticons.join(" ")
+  }
+
+  const on_mouse_enter_emoticons: React.MouseEventHandler = () => {
+    set_emoticon_is_mouse_over(true)
+    if (emoticon_timer === undefined) set_emoticons(random_emoticons(emoticons.split(" ")))
+
+    set_emoticon_timer(setInterval(() => {
+      set_emoticons(random_emoticons(emoticons.split(" ")))
+    },
+      500
+    ))
+  }
+
+  const on_mouse_leave_emoticons: React.MouseEventHandler = () => {
+    set_emoticon_is_mouse_over(false)
+    if (emoticon_timer === undefined) return
+    clearTimeout(emoticon_timer)
+    set_emoticon_timer(undefined)
+  }
+
   return (
     <div className="w-full border-t border-brand-gray-700 text-brand-gray-200 italic">
       <div className="relative sm:flex justify-center w-full max-w-6xl mx-auto py-4 px-6">
-        <div className="hidden sm:flex items-center absolute top-0 bottom-0 left-0 w-fit px-6 border-r border-brand-gray-700">
-          <p className="text-sm not-italic">:) :( :/ :O</p>
+        <div className="hidden sm:flex items-center absolute top-0 bottom-0 left-0 w-fit px-6 border-r border-brand-gray-700" onMouseEnter={on_mouse_enter_emoticons} onMouseLeave={on_mouse_leave_emoticons}>
+          <p className="text-sm not-italic">{emoticons}</p>
         </div>
 
         <p className="flex flex-row items-center">
