b35fb679f224c00a4e892dc2f7d9191c74fbf471
maxcaplan <maxacaplan@gmail.com>
2025-12-11 11:55:41 -0400

    Fixed type error

--- src/utils/noise/perlin.ts
+++ src/utils/noise/perlin.ts
@@ -4,7 +4,7 @@
 
 // This is free and unencumbered software released into the public domain
 
-function shuffleSeed(seed: Uint32Array): Uint32Array {
+function shuffleSeed(seed: Uint32Array<ArrayBuffer>): Uint32Array<ArrayBuffer> {
   const newSeed = new Uint32Array(1);
   newSeed[0] = seed[0] * 1664525 + 1013904223;
   return newSeed;
